3b15566012cc5451bd49ea6017c5838fcb4564e6
Andrew Wang <zxwandrew@gmail.com>
2016-03-19 17:57:14 -0700

    animations

--- src/app/layouts/CoreLayout.js
+++ src/app/layouts/CoreLayout.js
@@ -1,5 +1,8 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+// import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
+
 import HeaderBar from '../components/HeaderBar'
 import FooterBar from '../components/FooterBar'
 import '../styles/core.scss'
@@ -11,7 +14,18 @@ class CoreLayout extends React.Component {
         <HeaderBar></HeaderBar>
         <div className='view-container'>
           <div className='view'>
-            {this.props.children}
+
+
+            <ReactCSSTransitionReplace
+              component="div"
+              transitionName="fade-wait"
+              transitionEnterTimeout={500} transitionLeaveTimeout={500}
+            >
+              {React.cloneElement(this.props.children, {
+                key: this.props.location.pathname
+              })}
+            </ReactCSSTransitionReplace>
+
           </div>
           <FooterBar></FooterBar>
         </div>
--- src/app/pages/HomePage.js
+++ src/app/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
 class HomePage extends React.Component {
   render () {
     return (
       <div>
         <h1>I build <span className='span-highlight'>Elegant</span>, <span className='span-highlight'>Impactful</span >, and <span className='span-highlight'>Geospatially Aware</span> web applications.</h1>
+        <h2> Click here to see a selection of my <span className='span-highlight'><Link to='/projects'>Projects</Link></span> </h2>
       </div>
     );
   }
